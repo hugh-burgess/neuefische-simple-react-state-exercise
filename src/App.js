08b868a87ebd1904d487dd0959f6295d9248e34e
Martin Pagels <office@martinpagels.de>
2021-04-04 01:42:56 +0200

    Add exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import home from "./home.svg";
+import fav from "./fav.svg";
+import settings from "./settings.svg";
+import profile from "./profile.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <NavBar />
+      <Input />
     </div>
   );
 }
 
 export default App;
+
+function Header() {
+  return <header>Title</header>;
+}
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <button>
+        <img src={home} className="App-logo" alt="logo" />
+      </button>
+      <button>
+        <img src={fav} className="App-logo" alt="logo" />
+      </button>
+      <button>
+        <img src={settings} className="App-logo" alt="logo" />
+      </button>
+      <button>
+        <img src={profile} className="App-logo" alt="logo" />
+      </button>
+    </div>
+  );
+}
+
+function Input() {
+  return <AnotherDiv />;
+}
+
+function AnotherDiv() {
+  return <YouGottaBeKiddingMeDiv />;
+}
+
+function YouGottaBeKiddingMeDiv() {
+  return (
+    <form>
+      <input
+        id="name"
+        name="name"
+        placeholder="Type here any title you like"
+      ></input>
+      <button>Change header title from input</button>
+    </form>
+  );
+}
