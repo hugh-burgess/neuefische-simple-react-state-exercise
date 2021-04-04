b54c6b70f75e60c978cf03a9c604d3a869ede2b9
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-05 01:46:00 +0200

    working

--- src/App.js
+++ src/App.js
@@ -3,53 +3,81 @@ import fav from "./fav.svg";
 import settings from "./settings.svg";
 import profile from "./profile.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [titleName, setTitleName] = useState("");
+
+  function handleOnClickNavBar(currywurst) {
+    setTitleName(currywurst);
+  }
+
+  function headerChange(currywurst) {
+    setTitleName(currywurst);
+  }
+
   return (
     <div className="App">
-      <Header />
-      <NavBar />
-      <Input />
+      <Header title={titleName} />
+      <NavBar onNavClick={handleOnClickNavBar} />
+      <Input onInputClick={headerChange} />
     </div>
   );
 }
 
 export default App;
 
-function Header() {
-  return <header>Title</header>;
+function Header({ title }) {
+  return <header>{title.toUpperCase()}</header>;
 }
 
-function NavBar() {
+function NavBar({ onNavClick }) {
+  function handleHomeClick() {
+    onNavClick("home");
+  }
+  function handleFavClick() {
+    onNavClick("favourites");
+  }
+  function handleSettingsClick() {
+    onNavClick("settings");
+  }
+  function handleProfileClick() {
+    onNavClick("profile");
+  }
   return (
     <div className="NavBar">
-      <button>
+      <button onClick={handleHomeClick}>
         <img src={home} className="App-logo" alt="logo" />
       </button>
-      <button>
+      <button onClick={handleFavClick}>
         <img src={fav} className="App-logo" alt="logo" />
       </button>
-      <button>
+      <button onClick={handleSettingsClick}>
         <img src={settings} className="App-logo" alt="logo" />
       </button>
-      <button>
+      <button onClick={handleProfileClick}>
         <img src={profile} className="App-logo" alt="logo" />
       </button>
     </div>
   );
 }
 
-function Input() {
-  return <AnotherDiv />;
+function Input({ onInputClick }) {
+  return <AnotherDiv onInputClick={onInputClick} />;
 }
 
-function AnotherDiv() {
-  return <YouGottaBeKiddingMeDiv />;
+function AnotherDiv({ onInputClick }) {
+  return <YouGottaBeKiddingMeDiv onInputClick={onInputClick} />;
 }
 
-function YouGottaBeKiddingMeDiv() {
+function YouGottaBeKiddingMeDiv({ onInputClick }) {
+  function handleButtonSubmit(event) {
+    event.preventDefault();
+    const input = event.target.name.value;
+    onInputClick(input);
+  }
   return (
-    <form>
+    <form onSubmit={handleButtonSubmit}>
       <input
         id="name"
         name="name"
